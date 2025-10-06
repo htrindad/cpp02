2d0f1f27f85fbea244d78a7837342640813ca045
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2025-10-06 17:58:27 +0100

    done

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:48:41 by htrindad          #+#    #+#             */
-/*   Updated: 2025/10/06 17:50:40 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/10/06 17:57:53 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 // CDO
 Point::Point() : x(Fixed(0)), y(Fixed(0)) {};
 Point::~Point() {};
-Point::Point(const float x, const float y) : this->x(Fixed(x)), this->y(Fixed(y)) {}; // Need to explicitly state "this" so that the compiler know the difference
+Point::Point(const float x, const float y) : x(Fixed(x)), y(Fixed(y)) {}; // Need to explicitly state "this" so that the compiler know the difference
+Point::Point(Point const &point) { *this = point; }
 Point &Point::operator=(Point const &point)
 {
-	~Point();
+	this->~Point();
 	new(this) Point(point.x.toFloat(), point.y.toFloat()); // destroying and recreating the point
 	return *this;
 }
-Point::Point(Point const &point) { *this = point; }
 
 // Methods
 
-Fixed Point::getX() const { return x.getFloat(); }
-Fixed Point::getY() const { return y.getFloat(); }
+Fixed Point::getX() const { return x.toFloat(); }
+Fixed Point::getY() const { return y.toFloat(); }
 
 std::ostream &operator<<(std::ostream &out, const Point &point)
 {
--- ex03/Point.h
+++ ex03/Point.h
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:41:50 by htrindad          #+#    #+#             */
-/*   Updated: 2025/10/06 17:45:02 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/10/06 17:54:04 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ class Point
 		~Point();
 		Point(const Point &point);
 		Point(const float x, const float y);
-		Point &operator=(Point &point);
+		Point &operator=(Point const &point);
 		Fixed getX() const;
 		Fixed getY() const;
 };
